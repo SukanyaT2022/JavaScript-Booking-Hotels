0d3dd76c2495a70288d117795e3d182adbc2e891
TiparpronS <96738451+SukanyaT2022@users.noreply.github.com>
2024-05-09 10:55:42 -0400

    add

--- index.js
+++ index.js
@@ -35,7 +35,37 @@ function standardRoom(price, quantity, roomImage, NumberBed, description) {
   }
 
 
-const hotel = new Hotel("hotelName", "location", "image");
+const hotelThailand = new Hotel("Lotus Thailand", "Thailand", "https://plus.unsplash.com/premium_photo-1683134041222-fb1cb31c1203?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8cGh1a2V0JTIwJTIwYmVhY2glMjAlMjByZXNvcnR8ZW58MHx8MHx8fDA%3D");
+const hotelDubai = new Hotel("Lotus Dubai", "Dubai", "https://plus.unsplash.com/premium_photo-1661962790754-9a1a5b5e334a?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8ZHViYWklMjBiZWFjaCUyMCUyMHJlc29ydHxlbnwwfHwwfHx8MA%3D%3D");
+const hotelHawaii = new Hotel("Lotus Hawaii", "Hawaii", "https://images.unsplash.com/photo-1646843132316-e63a43d8a251?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGhhd2FpaSUyMGJlYWNoJTIwJTIwcmVzb3J0fGVufDB8fDB8fHww");
+
+var hotel = [hotelThailand,hotelDubai,hotelHawaii ]
+
+function displayHotel(){
+  var readListBoxHotel = document.getElementById(allHotelID).value
+  //go to all loop of hotel object
+  for (i=0;i<hotel.length;i++){
+if(hotel[i].hotelName == readListBoxHotel){
+  var createDiv = document.createElement("div")
+  var createPtag = document.createElement("p")
+  var textNote = document.createTextNode(hotel[i].hotelName)//crate text  inside p tag
+  createPtag.appendChild(textNote)//put textnote on ptag
+  createDiv.appendChild(createPtag)//put ptag in div 
+
+//create image tag
+  var imgTag = document.createElement("img")//we create image tag
+imgTag.setAttribute("src",hotel[i].image)//from hotel object
+imgTag.setAttribute("height","150px")
+imgTag.setAttribute("width","150px")
+createDiv.appendChild(imgTag) //add image tag to div tag
+document.getElementById('displayHotelImage').appendChild(createDiv)
+}
+  }
+}
+
+
+
+
 const sweetRoom = new SweetRoom(400, 4, "roomImage", 1, "description");
 const familyRoom = new FamilyRoom(300, 10, "roomImage", 3, "description");
 const standardRoom = new StandardRoom(200, 20, "roomImage", 2, "description");
